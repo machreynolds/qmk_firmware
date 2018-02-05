6338918b65011e18b5de7fc96d1eb207fe3a8034
Scott Wallace <scottwallace.dev@gmail.com>
2018-02-05 10:14:34 +0000

    Add capslock

--- keyboards/lets_split/keymaps/scott/keymap.c
+++ keyboards/lets_split/keymaps/scott/keymap.c
@@ -44,14 +44,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Sft\  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |SftEtr|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | Gui  | Nav  |Lower |Space |Space |Raise | Nav  | AltGr| Down |Mouse |
+ * | Ctrl | Alt  | Gui  | Nav  |Lower |Space |Space |Raise | Nav  | AltGr| Caps |Mouse |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   SFT_BLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT, \
-  KC_LCTL, KC_LALT, KC_LGUI, NAV,     LOWER,   RAI_SPC, LOW_SPC, RAISE,   NAV,     KC_RALT, KC_DOWN, MOUSE    \
+  KC_LCTL, KC_LALT, KC_LGUI, NAV,     LOWER,   RAI_SPC, LOW_SPC, RAISE,   NAV,     KC_RALT, KC_CAPS, MOUSE    \
 ),
 
 /* Lower
